2a86f264d57d9f8f89109b0eb110a287036e2ac0
Xavier Yao <xavieryao@live.cn>
2014-06-29 13:07:13 +0800

    Add version name onto AboutDialog

--- app/src/main/java/org/papdt/goodnight/AboutDialog.java
+++ app/src/main/java/org/papdt/goodnight/AboutDialog.java
@@ -3,6 +3,7 @@ package org.papdt.goodnight;
 import android.app.AlertDialog;
 import android.app.Dialog;
 import android.app.DialogFragment;
+import android.content.pm.PackageManager;
 import android.graphics.Typeface;
 import android.os.Bundle;
 import android.text.Spannable;
@@ -38,6 +39,8 @@ public class AboutDialog extends DialogFragment {
         quote.setSpan(new StyleSpan(Typeface.ITALIC), 0,
                 quote.length(), Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
         sb.append(quote);
+        sb.append(getString(R.string.app_name));
+        sb.append(String.format("\nver.%s\n\n", getString(R.string.version_name)));
         sb.append(getString(R.string.copyright));
         SpannableString cc_url = new SpannableString(getString(R.string.audio_license));
         cc_url.setSpan(new URLSpan(CC_URL),0,cc_url.length(), Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
